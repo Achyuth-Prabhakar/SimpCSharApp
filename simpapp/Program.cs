c6fd870eb6bcea969724126ae16904f2ae38b572
Achyuth-Prabhakar <achyuth.emidi@gmail.com>
2023-05-18 10:38:17 +0530

    Add project files.

---
+++ simpapp/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static int Main(string[] args)
+    {
+        Console.WriteLine("****APP***");
+        Console.WriteLine("Hello World");
+        Console.WriteLine();
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            Console.WriteLine("Arg: {0}", args[i]);
+        }
+        Console.ReadLine();
+
+        return 0;
+    }
+}
